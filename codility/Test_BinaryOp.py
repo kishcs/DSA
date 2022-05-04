4f1bece09d0ccc5334a0cb92cf93496c04d9549c
Kishore Chandra Sahoo <33931003+kishcs@users.noreply.github.com>
2022-05-04 08:59:16 +0530

    Update Test_BinaryOp.py

--- codility/Test_BinaryOp.py
+++ codility/Test_BinaryOp.py
@@ -2,54 +2,83 @@
 import time
 
 
-# time taken: 8154603300
-# 799999
 def solution(S):
     ops = 0
     a = int(S, 2)
-    # print(a)
     while a:
         if a & 1:  # odd
-            a -= 1  # if in even case you are going to shift, then no need to do minus 1
-            # e.g. 11101 >> 1 is same as 11100 >> 1
+            a -= 1
         else:
-            a >>= 1  # even so divide by 2.. same as right shift
+            a >>= 1  # even: divide by 2 => same as right shift
         ops += 1
-    print('==>', ops)
+    # print('==>', ops)
     return ops
 
 
-# time taken: 4603804400
-# 799999
-def solution1(S):  # 100% Solution
+def solution1(S):
+    """
+    In ODD case, reduce by 1, then in EVEN case divide by 2 (same as right shift by 1)
+    If we will perform rightshift for next EVEN, then no need to reduce by 1 for prev ODD num.
+    because right shifting N(i.e. ODD) with 1 bit is same as right shifting (N-1)(i.e. EVEN) with 1 bit
+    e.g. 11101 >> 1 is same as 11100 >> 1, both ends up at 1110
+    """
     ops = 0
     a = int(S, 2)
     while a:
-        if 1 < a and a & 1:
+        if a & 1 and a > 1:  # odd but not 1
             ops += 1
         a >>= 1
         ops += 1
-        print(a)
+        # print(a)
     return ops
 
 
-if __name__ == "__main__":
-    # s = '1111010101111'
-    s = '1' * 400000
+def solution2(S):  # 100% Solution
+    # Formula: ops = 2X-1+Y, X: No. of Ones, Y: No. of Zeros
+    data = str(S)
+    ones = data.count('1')
+    zeros = data.count('0')
+    # print(ones, zeros)
+    return 2 * ones - 1 + zeros
+
+
+def main():
+    # s = '111101010111010'
+    s = '1' * 1000000
     # s = '11100'
-    # op = solution1(s)
-    # print(op)
-    # print(2>>1, 1>>1)
 
     start = time.time_ns()
     op = solution(s)
-    print('time taken:', (time.time_ns() - start))
+    print('time taken:', (time.time_ns() - start) / 1000000, 'ms')
     print(op)
 
     start = time.time_ns()
     op = solution1(s)
-    print('time taken:', (time.time_ns() - start))
+    print('time taken:', (time.time_ns() - start) / 1000000, 'ms')
     print(op)
 
-    # print(int('11100', 2), int('11101', 2), 28>>1, 29>>1)
-    # print(6 >> 1, 7 >> 1)
+    start = time.time_ns()
+    op = solution2(s)
+    print('time taken:', (time.time_ns() - start) / 1000000, 'ms')
+    print(op)
+
+
+def stressTest():
+    data = ['1' * 400000, '1' * 600000, '1' * 800000, '1' * 1000000]
+    # print(data)
+    start = time.time_ns()
+    for s in data:
+        op = solution1(s)
+        print(op)
+    print('Total time taken:', (time.time_ns() - start) / 1000000, 'ms')
+
+    start = time.time_ns()
+    for s in data:
+        op = solution2(s)
+        print(op)
+    print('Total time taken:', (time.time_ns() - start) / 1000000, 'ms')
+
+
+if __name__ == "__main__":
+    main()
+    # stressTest()
